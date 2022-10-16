4e0b5434dbc01e5988c2a6b40b48a3d25e4ae9e7
Natan A. Costa <natanarcosta@gmail.com>
2022-10-16 14:45:39 -0300

    heroku settings

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
