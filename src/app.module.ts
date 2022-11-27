9e36b7809e7c12e432acf292a8093e911a691fc9
Natan A. Costa <natanarcosta@gmail.com>
2022-11-27 11:04:39 -0300

    Added cron to ping bot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { DiscordModule } from '@discord-nestjs/core';
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { GatewayIntentBits } from 'discord.js';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotGateway } from './discord-bot/discord-bot.gateway';
 import { DiscordBotModule } from './discord-bot/discord-bot.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
     DiscordModule.forRootAsync({
       useFactory: () => ({
         token: process.env.TOKEN,
@@ -25,6 +28,7 @@ import { DiscordBotModule } from './discord-bot/discord-bot.module';
       }),
     }),
     DiscordBotModule,
+    HttpModule,
   ],
   controllers: [AppController],
   providers: [AppService, BotGateway],
