d734ca9d394be8db4b9c238eab27f1d8bd5a5948
Natan A. Costa <natanarcosta@gmail.com>
2022-11-27 11:13:32 -0300

    Updated app version

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,12 +2,13 @@ import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { firstValueFrom } from 'rxjs';
+const pjson = require('../package.json');
 
 @Injectable()
 export class AppService {
   constructor(private http: HttpService) {}
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + ' v' + pjson.version;
   }
 
   @Cron(CronExpression.EVERY_10_MINUTES)
